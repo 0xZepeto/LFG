1704abb43522747f0594831282dc6ad1866cd302
0xZepeto <72789671+0xZepeto@users.noreply.github.com>
2025-09-04 03:04:04 +0700

    Create useDebounce.ts

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+
+/**
+ * Debounce hook to delay function execution
+ * @param value Value to debounce
+ * @param delay Delay in milliseconds
+ * @returns Debounced value
+ */
+export function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+/**
+ * Debounce hook for callbacks
+ * @param callback Function to debounce
+ * @param delay Delay in milliseconds
+ * @returns Debounced function
+ */
+export function useDebouncedCallback<T extends (...args: any[]) => any>(
+  callback: T,
+  delay: number
+): T {
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [timeoutId]);
+
+  return ((...args: Parameters<T>) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    const id = setTimeout(() => {
+      callback(...args);
+    }, delay);
+
+    setTimeoutId(id);
+  }) as T;
+}
